158ba02a26a43cf0fc6d64cbd94fa36497393887
Magun <francisco.iri_95@hotmail.es>
2019-01-31 15:24:23 -0300

    login listo

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,9 +3,11 @@ import {Router} from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { ServiciosService } from '../../services/servicios.service';
 import {NgForm, NgModel} from '@angular/forms';
-import {Login} from '../../interfaces/modelos.interface';
+import { Login } from '../../interfaces/modelos.interface';
 import { first } from 'rxjs/operators';
 import { FormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { userInfo } from 'os';
+import { Key } from 'protractor';
 
 
 @Component({
@@ -14,12 +16,13 @@ import { FormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  apiKey:string;
+  key:any[]=[];
   paises: any[]=[];
 
   user:Login ={
     rut:"",
     password:""
+
   }
 
  
@@ -33,14 +36,40 @@ export class LoginComponent implements OnInit {
         console.log(data);
       })
      }
+
+
+  getIp(ip:string){
+    this.user.apikey=ip;
+    console.log(this.user.apikey);
+
+  }
  
 
    logiar(){
     console.log(this.user);
-    this.servicio.ingresar(this.user);
+    this.servicio.ingresar(this.user)
+    .subscribe((data: any) =>{
+      console.log("antes de la asignacion");
+      console.log(this.key);    
+      this.key=data.apiKey;
+      this.user.apikey=data.apiKey;
+      console.log("despues de la asignacion");
+      console.log(this.key);
+      console.log(this.user.apikey);  
+      
+    },
+    error => {alert("Datos Invalidos");
+      console.error(error);
+      
+    }
+           
+      
+    );    
     
     //this.router.navigate(['home']);
   }
+
+  
 
   ngOnInit() {
   }
--- src/app/services/servicios.service.ts
+++ src/app/services/servicios.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Login } from '../interfaces/modelos.interface';
+import {Observable} from 'rxjs/internal/Observable';
+import {map} from 'rxjs/operators';
 
 import 'rxjs';
 //import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
@@ -10,11 +12,16 @@ import 'rxjs';
 })
 export class ServiciosService {
   URL:String = "https://api.sebastian.cl/academia/";
+
+  ip:any[]=[];
   
 
   constructor(private http: HttpClient) { 
     console.log('En el servicio.service');
   }
+  headers =new HttpHeaders({
+    'Content-Type':'application/json'
+  });
 
   /*canActivate(){
     return true;
@@ -28,11 +35,26 @@ export class ServiciosService {
     });
 
     return this.http.post(this.URL+'api/v1/authentication/authenticate',body,{headers})
-    .subscribe(res =>{
-        console.log(res);
-        return res;
-    })
+    .pipe(map((res: any )=>{ 
+      this.ip=res.apikey;
+      console.log(this.ip);
+      console.log(res);    
+      return res;          
+    }
+    
+      ))
   };
+  
+  
+
+  agregarIp(){
+    console.log("Agregar IPK")
+  }
+
+  /*saveKey(){
+    return this.http.post(this.URL+'api/v1/authentication/authenticate',body,{headers:this.headers})
+    .pipe(map(data =>data));
+  }*/
 
   /*ingresar(user:Login){
     let body =JSON.stringify(user);
